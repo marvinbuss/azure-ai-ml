e48a2e3ced4d81d3e631c4c9e0c53bd6ea46261b
Marvin Buss <marvin.buss@gmail.com>
2023-09-12 18:29:17 +0200

    Add storage

--- code/terraform/roleassignments.tf
+++ code/terraform/roleassignments.tf
@@ -1,3 +1,9 @@
+resource "azurerm_role_assignment" "current_roleassignment_storage" {
+  scope                = azurerm_storage_account.storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
 resource "azurerm_role_assignment" "uai_role_assignment_resource_group_reader" {
   scope                = data.azurerm_resource_group.resource_group.id
   role_definition_name = "Reader"
