657393ea65b601f310db8a8e5194ee213c3f68f1
Marvin Buss <marvin.buss@gmail.com>
2023-09-12 16:00:43 +0200

    Add missing compute

--- code/terraform/machinelearningcompute.tf
+++ code/terraform/machinelearningcompute.tf
@@ -1,4 +1,33 @@
 resource "azurerm_machine_learning_compute_cluster" "machine_learning_compute_cluster" {
+  machine_learning_workspace_id = azurerm_machine_learning_workspace.machine_learning_workspace.id
+  name                          = "builder001"
+  location                      = var.location
+  resource_group_name           = data.azurerm_resource_group.resource_group.name
+  tags                          = var.tags
+  identity {
+    type = "UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.user_assigned_identity.id
+    ]
+  }
+
+  description            = "Compute Clutser to build Container Images"
+  local_auth_enabled     = false
+  node_public_ip_enabled = false
+  scale_settings {
+    min_node_count = 0
+    max_node_count = 1
+  }
+  ssh_public_access_enabled = false
+  vm_priority               = "Dedicated"
+  vm_size                   = "Standard_DS2_v2"
+
+  depends_on = [
+    azapi_update_resource.machine_learning_managed_network
+  ]
+}
+
+resource "azurerm_machine_learning_compute_cluster" "machine_learning_compute_cluster" {
   for_each = var.machine_learning_compute_clusters
 
   machine_learning_workspace_id = azurerm_machine_learning_workspace.machine_learning_workspace.id
