6b936944119557f3a4faaaa083e25b0882826eba
Marvin Buss <marvin.buss@gmail.com>
2023-09-12 16:01:33 +0200

    Fix wrong reference in locals

--- code/terraform/locals.tf
+++ code/terraform/locals.tf
@@ -2,8 +2,8 @@ locals {
   prefix = "${lower(var.prefix)}-${var.environment}"
 
   subnet = {
-    resource_group_name  = split("/", var.vnet_id)[4]
-    virtual_network_name = split("/", var.vnet_id)[8]
-    name                 = split("/", var.vnet_id)[10]
+    resource_group_name  = split("/", var.subnet_id)[4]
+    virtual_network_name = split("/", var.subnet_id)[8]
+    name                 = split("/", var.subnet_id)[10]
   }
 }
