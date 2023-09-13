aa06f1062965f9b16cb1bbbd9d22ead192245bb0
Marvin Buss <marvin.buss@gmail.com>
2023-09-13 10:19:51 +0200

    Add Aml connection for search

---
+++ code/terraform/machinelearningconnections.tf
@@ -0,0 +1,19 @@
+resource "azapi_resource" "machine_learning_workspace_connection_search" {
+  count = var.search_service_enabled ? 1 : 0
+
+  type      = "Microsoft.MachineLearningServices/workspaces/connections@2023-06-01-preview"
+  name      = azurerm_search_service.search_service.name
+  parent_id = azurerm_machine_learning_workspace.machine_learning_workspace.id
+
+  body = jsonencode({
+    properties = {
+      authType = "ApiKey"
+      category = "CognitiveSearch"
+      credentials = {
+        key = azurerm_search_service.search_service.primary_key
+      }
+      metadata = {}
+      target   = "https://${azurerm_search_service.search_service.name}.cognitiveservices.azure.com/"
+    }
+  })
+}
