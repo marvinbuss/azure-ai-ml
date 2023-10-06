a633a4102763bc1fa929df7b81160d03e51b495b
Marvin Buss <marvin.buss@gmail.com>
2023-10-06 12:30:16 +0200

    Test

--- code/terraform/machinelearning.tf
+++ code/terraform/machinelearning.tf
@@ -1,63 +1,7 @@
-# resource "azurerm_machine_learning_workspace" "machine_learning_workspace" {
-#   name                = "${local.prefix}-mlw001"
-#   location            = var.location
-#   resource_group_name = data.azurerm_resource_group.resource_group.name
-#   tags                = var.tags
-#   identity {
-#     type = "UserAssigned"
-#     identity_ids = [
-#       azurerm_user_assigned_identity.user_assigned_identity.id
-#     ]
-#   }
-
-#   application_insights_id        = azurerm_application_insights.application_insights.id
-#   container_registry_id          = azurerm_container_registry.container_registry.id
-#   key_vault_id                   = azurerm_key_vault.key_vault.id
-#   storage_account_id             = azurerm_storage_account.storage.id
-#   description                    = "Azure Machine Learning Workspace for environment ${var.environment} with prefix ${var.prefix}."
-#   friendly_name                  = title(replace("${local.prefix}-mlw001", "-", " "))
-#   high_business_impact           = true
-#   image_build_compute_name       = local.default_machine_learning_workspace_image_builder_compute_name
-#   primary_user_assigned_identity = azurerm_user_assigned_identity.user_assigned_identity.id
-#   public_network_access_enabled  = false
-#   sku_name                       = "Basic"
-#   v1_legacy_mode_enabled         = false
-
-#   depends_on = [
-#     azurerm_role_assignment.uai_role_assignment_resource_group_reader,
-#     azurerm_role_assignment.uai_role_assignment_container_registry_contributor,
-#     azurerm_role_assignment.uai_role_assignment_storage_contributor,
-#     azurerm_role_assignment.uai_role_assignment_storage_blob_contributor,
-#     azurerm_role_assignment.uai_role_assignment_key_vault_contributor,
-#     azurerm_role_assignment.uai_role_assignment_key_vault_administrator,
-#     azurerm_role_assignment.uai_role_assignment_application_insights_contributor
-#   ]
-# }
-
-# resource "azapi_update_resource" "machine_learning_managed_network" {
-#   type        = "Microsoft.MachineLearningServices/workspaces@2023-06-01-preview"
-#   resource_id = azurerm_machine_learning_workspace.machine_learning_workspace.id
-
-#   body = jsonencode({
-#     properties = {
-#       managedNetwork = {
-#         isolationMode = "AllowOnlyApprovedOutbound"
-#         status = {
-#           status     = "Active"
-#           sparkReady = true
-#         }
-#         outboundRules = local.machine_learning_workspace_outbound_rules
-#       }
-#       systemDatastoresAuthMode = "identity"
-#     }
-#   })
-# }
-
-resource "azapi_resource" "machine_learning_workspace" {
-  type = "Microsoft.MachineLearningServices/workspaces@2023-06-01-preview"
+resource "azurerm_machine_learning_workspace" "machine_learning_workspace" {
   name                = "${local.prefix}-mlw001"
   location            = var.location
-  parent_id = data.azurerm_resource_group.resource_group.id
+  resource_group_name = data.azurerm_resource_group.resource_group.name
   tags                = var.tags
   identity {
     type = "UserAssigned"
@@ -65,50 +9,106 @@ resource "azapi_resource" "machine_learning_workspace" {
       azurerm_user_assigned_identity.user_assigned_identity.id
     ]
   }
-  
+
+  application_insights_id        = azurerm_application_insights.application_insights.id
+  container_registry_id          = azurerm_container_registry.container_registry.id
+  key_vault_id                   = azurerm_key_vault.key_vault.id
+  storage_account_id             = azurerm_storage_account.storage.id
+  description                    = "Azure Machine Learning Workspace for environment ${var.environment} with prefix ${var.prefix}."
+  friendly_name                  = title(replace("${local.prefix}-mlw001", "-", " "))
+  high_business_impact           = true
+  image_build_compute_name       = local.default_machine_learning_workspace_image_builder_compute_name
+  primary_user_assigned_identity = azurerm_user_assigned_identity.user_assigned_identity.id
+  public_network_access_enabled  = false
+  sku_name                       = "Basic"
+  v1_legacy_mode_enabled         = false
+
+  depends_on = [
+    azurerm_role_assignment.uai_role_assignment_resource_group_reader,
+    azurerm_role_assignment.uai_role_assignment_container_registry_contributor,
+    azurerm_role_assignment.uai_role_assignment_storage_contributor,
+    azurerm_role_assignment.uai_role_assignment_storage_blob_contributor,
+    azurerm_role_assignment.uai_role_assignment_key_vault_contributor,
+    azurerm_role_assignment.uai_role_assignment_key_vault_administrator,
+    azurerm_role_assignment.uai_role_assignment_application_insights_contributor
+  ]
+}
+
+resource "azapi_update_resource" "machine_learning_managed_network" {
+  type        = "Microsoft.MachineLearningServices/workspaces@2023-06-01-preview"
+  resource_id = azurerm_machine_learning_workspace.machine_learning_workspace.id
+
   body = jsonencode({
-    kind = "Default"
-    sku = {
-      name = "Basic"
-      tier = "Basic"
-    }
     properties = {
-      applicationInsights = azurerm_application_insights.application_insights.id
-      containerRegistry = azurerm_container_registry.container_registry.id
-      keyVault = azurerm_key_vault.key_vault.id
-      storageAccount = azurerm_storage_account.storage.id
-      allowPublicAccessWhenBehindVnet = false
-      associatedWorkspaces = []
-      description = "Azure Machine Learning Workspace for environment ${var.environment} with prefix ${var.prefix}."
-      enableDataIsolation = true
-      existingWorkspaces = []
-      friendlyName = title(replace("${local.prefix}-mlw001", "-", " "))
-      hbiWorkspace = true
-      imageBuildCompute = local.default_machine_learning_workspace_image_builder_compute_name
       managedNetwork = {
         isolationMode = "AllowOnlyApprovedOutbound"
         status = {
+          status     = "Active"
           sparkReady = true
-          status = "Active"
         }
         outboundRules = local.machine_learning_workspace_outbound_rules
       }
-      publicNetworkAccess = "Disabled"
-      primaryUserAssignedIdentity = azurerm_user_assigned_identity.user_assigned_identity.id
-      softDeleteRetentionInDays = 7
       systemDatastoresAuthMode = "identity"
-      v1LegacyMode = false
     }
   })
 }
 
+# resource "azapi_resource" "machine_learning_workspace" {
+#   type = "Microsoft.MachineLearningServices/workspaces@2023-06-01-preview"
+#   name                = "${local.prefix}-mlw001"
+#   location            = var.location
+#   parent_id = data.azurerm_resource_group.resource_group.id
+#   tags                = var.tags
+#   identity {
+#     type = "UserAssigned"
+#     identity_ids = [
+#       azurerm_user_assigned_identity.user_assigned_identity.id
+#     ]
+#   }
+  
+#   body = jsonencode({
+#     kind = "Default"
+#     sku = {
+#       name = "Basic"
+#       tier = "Basic"
+#     }
+#     properties = {
+#       applicationInsights = azurerm_application_insights.application_insights.id
+#       containerRegistry = azurerm_container_registry.container_registry.id
+#       keyVault = azurerm_key_vault.key_vault.id
+#       storageAccount = azurerm_storage_account.storage.id
+#       allowPublicAccessWhenBehindVnet = false
+#       associatedWorkspaces = []
+#       description = "Azure Machine Learning Workspace for environment ${var.environment} with prefix ${var.prefix}."
+#       enableDataIsolation = true
+#       existingWorkspaces = []
+#       friendlyName = title(replace("${local.prefix}-mlw001", "-", " "))
+#       hbiWorkspace = true
+#       imageBuildCompute = local.default_machine_learning_workspace_image_builder_compute_name
+#       managedNetwork = {
+#         isolationMode = "AllowOnlyApprovedOutbound"
+#         status = {
+#           sparkReady = true
+#           status = "Active"
+#         }
+#         outboundRules = local.machine_learning_workspace_outbound_rules
+#       }
+#       publicNetworkAccess = "Disabled"
+#       primaryUserAssignedIdentity = azurerm_user_assigned_identity.user_assigned_identity.id
+#       softDeleteRetentionInDays = 7
+#       systemDatastoresAuthMode = "identity"
+#       v1LegacyMode = false
+#     }
+#   })
+# }
+
 data "azurerm_monitor_diagnostic_categories" "diagnostic_categories_machine_learning_workspace" {
-  resource_id = azapi_resource.machine_learning_workspace.id
+  resource_id = azurerm_machine_learning_workspace.machine_learning_workspace.id
 }
 
 resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_machine_learning_workspace" {
   name                       = "logAnalytics"
-  target_resource_id         = azapi_resource.machine_learning_workspace.id
+  target_resource_id         = azurerm_machine_learning_workspace.machine_learning_workspace.id
   log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
 
   dynamic "enabled_log" {
@@ -130,23 +130,23 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting_machine_learni
 }
 
 resource "azurerm_private_endpoint" "machine_learning_workspace_private_endpoint" {
-  name                = "${azapi_resource.machine_learning_workspace.name}-pe"
+  name                = "${azurerm_machine_learning_workspace.machine_learning_workspace.name}-pe"
   location            = var.location
-  resource_group_name = azapi_resource.machine_learning_workspace.resource_group_name
+  resource_group_name = azurerm_machine_learning_workspace.machine_learning_workspace.resource_group_name
   tags                = var.tags
 
-  custom_network_interface_name = "${azapi_resource.machine_learning_workspace.name}-nic"
+  custom_network_interface_name = "${azurerm_machine_learning_workspace.machine_learning_workspace.name}-nic"
   private_service_connection {
-    name                           = "${azapi_resource.machine_learning_workspace.name}-pe"
+    name                           = "${azurerm_machine_learning_workspace.machine_learning_workspace.name}-pe"
     is_manual_connection           = false
-    private_connection_resource_id = azapi_resource.machine_learning_workspace.id
+    private_connection_resource_id = azurerm_machine_learning_workspace.machine_learning_workspace.id
     subresource_names              = ["amlworkspace"]
   }
   subnet_id = data.azurerm_subnet.subnet.id
   dynamic "private_dns_zone_group" {
     for_each = var.private_dns_zone_id_machine_learning_api == "" || var.private_dns_zone_id_machine_learning_notebooks == "" ? [] : [1]
     content {
-      name = "${azapi_resource.machine_learning_workspace.name}-arecord"
+      name = "${azurerm_machine_learning_workspace.machine_learning_workspace.name}-arecord"
       private_dns_zone_ids = [
         var.private_dns_zone_id_machine_learning_api,
         var.private_dns_zone_id_machine_learning_notebooks
